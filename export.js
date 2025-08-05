a122c8f5b5c77576b1e2b88d3b345b0c024f2f80
Денис Сидоров <denisidopost@yandex.ru>
2025-08-05 17:09:52 +0300

    05-07-2025_17-09 Начало начал!

--- export.js
+++ export.js
@@ -8,7 +8,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 (async () => {
   const browser = await puppeteer.launch({
     headless: true,
-    defaultViewport: { width: 800, height: 600 }
+    defaultViewport: { width: 1024, height: 1024 }
   });
 
   const page = await browser.newPage();
